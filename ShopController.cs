9a022c54ed5ec8079d219014cf399bac0d01f892
u17185018 <52907161+u17185018@users.noreply.github.com>
2019-07-18 14:18:28 +0200

    Add files via upload

---
+++ ShopController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework7.Controllers
+{
+    public class ShopController : Controller
+    {
+
+
+        //List<ViewModels.ShopItemViewModel> shop = new List<ViewModels.ShopItemViewModel>(); 
+        public static  ViewModels.ShopViewModel Shop = new ViewModels.ShopViewModel();
+        // GET: Shop
+        public ActionResult Index()
+        {
+         
+            return View(Shop);
+        }
+        public ActionResult AddItem()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddItem(string fName,string fDescription,double fPrice,int fQuantity)
+        {
+
+            ViewModels.ShopItemViewModel item = new ViewModels.ShopItemViewModel(fName,fDescription,fPrice,fQuantity);
+            Shop.AddItem(item);
+            return View("Index",Shop);
+        }
+    }
+}
---
+++ ShopItemViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework7.ViewModels
+{
+    public class ShopItemViewModel
+    {
+        private string mName;
+        private string mDescription;
+        private double mPrice;
+        private int mQuantitiyAvailable;
+
+        public string Name
+        {
+            get { return mName; }
+            set { mName = value; }
+    }
+        public string Description
+        {
+            get { return mDescription; }
+            set { mDescription = value; }
+        }
+        public double Price
+        {
+            get { return mPrice; }
+            set { mPrice = value; }
+        }
+        public int QuantityAvailable
+        {
+            get { return mQuantitiyAvailable; }
+            set { mQuantitiyAvailable = value; }
+        }
+        public ShopItemViewModel()
+          {
+
+
+           }
+
+
+        public ShopItemViewModel(string fName, string fDesc,double fPrice,int fQuantity)
+        {
+            Name = fName;
+            Description = fDesc;
+            Price = fPrice;
+            QuantityAvailable = fQuantity;
+        }
+    }
+}
